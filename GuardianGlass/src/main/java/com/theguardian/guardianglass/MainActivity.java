ee4ec84c5e5785880b9e15f45e2271f60a0886f9
rbates <rupert.bates@gmail.com>
2014-01-16 16:54:40 +0000

    implement swiping

--- GuardianGlass/src/main/java/com/theguardian/guardianglass/MainActivity.java
+++ GuardianGlass/src/main/java/com/theguardian/guardianglass/MainActivity.java
@@ -1,42 +1,53 @@
 package com.theguardian.guardianglass;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
+import com.squareup.picasso.Picasso;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
-import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends Activity {
+    public static final String LOG_TAG = "GuardianGlass";
     private TextView titleTextView;
     private TextView trailTextView;
     private GuardianGroup group;
     private ImageView imageView;
+    private GestureDetector gestureDetector;
+    private int currentCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface tf = getFont();
         titleTextView = (TextView) findViewById(R.id.title);
+        titleTextView.setTypeface(tf);
         titleTextView.setText("Fetching top story");
         trailTextView = (TextView) findViewById(R.id.trail);
+        trailTextView.setTypeface(tf);
         trailTextView.setText("");
         imageView = (ImageView) findViewById(R.id.image);
         imageView.setBackgroundDrawable(null);
+        gestureDetector = createGestureDetector(this);
         new FetchCardTask().execute();
     }
 
@@ -47,11 +58,68 @@ public class MainActivity extends Activity {
         return true;
     }
 
+
+    private GestureDetector createGestureDetector(Context context) {
+        GestureDetector gestureDetector = new GestureDetector(context);
+        //Create a base listener for generic gestures
+        gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
+            @Override
+            public boolean onGesture(Gesture gesture) {
+                if (gesture == Gesture.TAP) {
+                    // do something on tap
+                    return true;
+                } else if (gesture == Gesture.TWO_TAP) {
+                    // do something on two finger tap
+                    return true;
+                } else if (gesture == Gesture.SWIPE_RIGHT) {
+                    swipeRight();
+                    return true;
+                } else if (gesture == Gesture.SWIPE_LEFT) {
+                    swipeLeft();
+                    return true;
+                }
+                return false;
+            }
+        });
+        gestureDetector.setFingerListener(new GestureDetector.FingerListener() {
+            @Override
+            public void onFingerCountChanged(int previousCount, int currentCount) {
+                // do something on finger count changes
+            }
+        });
+        gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
+            @Override
+            public boolean onScroll(float displacement, float delta, float velocity) {
+                // do something on scrolling
+                return true;
+            }
+        });
+        return gestureDetector;
+    }
+
+    private void swipeLeft() {
+        if(currentCard == 0)
+            return;
+        currentCard--;
+        setCardContent(currentCard);
+
+    }
+
+    private void swipeRight() {
+        if(currentCard >= group.cards.length)
+            return;
+        currentCard++;
+        setCardContent(currentCard);
+
+    }
+
+    /*
+     * Send generic motion events to the gesture detector
+     */
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
-            openOptionsMenu();
-            return true;
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        if (gestureDetector != null) {
+            return gestureDetector.onMotionEvent(event);
         }
         return false;
     }
@@ -71,23 +139,27 @@ public class MainActivity extends Activity {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
         //shareIntent.putExtra(Intent.EXTRA_SUBJECT, getCard().title);
-        StringBuilder text = new StringBuilder(getCard().title);
+        StringBuilder text = new StringBuilder(getCard(currentCard).title);
         text.append("\n\n");
-        text.append(getUrl(getCard()));
+        text.append(getUrl(getCard(currentCard)));
 
         shareIntent.putExtra(Intent.EXTRA_TEXT, text.toString());
         startActivity(Intent.createChooser(shareIntent, getString(R.string.share_story_title)));
     }
 
-    private GuardianCard getCard() {
-        return group.cards[0];
+    private GuardianCard getCard(int i) {
+        return group.cards[i];
     }
 
     private String getUrl(GuardianCard card) {
         return card.uri.replace("http://mobile-apps.guardianapis.com/uk/items/", "http://theguardian.com/");
     }
 
-    class FetchCardTask extends AsyncTask<String, Integer, GuardianGroup>{
+    public Typeface getFont(){
+        return Typeface.createFromAsset(getAssets(), "GdnEgyDE2Lig.otf");
+    }
+
+    class FetchCardTask extends AsyncTask<String, Integer, GuardianGroup> {
 
         @Override
         protected GuardianGroup doInBackground(String... params) {
@@ -109,20 +181,27 @@ public class MainActivity extends Activity {
         }
 
 
-
         @Override
         protected void onPostExecute(GuardianGroup result) {
-            if(result == null)
+            if (result == null)
                 titleTextView.setText("Couldn't load card");
-            else{
+            else {
                 group = result;
-                titleTextView.setText(getCard().title);
-                trailTextView.setText(Html.fromHtml(getCard().trailText));
-                Picasso.with(MainActivity.this)
-                        .load(getCard().getImageUri())
-                        .into(imageView);
-                imageView.setAlpha(0.5f);
+                setCardContent(0);
             }
         }
+    }
+
+    private void setCardContent(int i) {
+        currentCard = i;
+        GuardianCard card = getCard(i);
+        titleTextView.setText(card.title);
+        trailTextView.setText(Html.fromHtml(card.trailText));
+        Picasso.with(MainActivity.this)
+                .load(card.getImageUri())
+                .placeholder(R.drawable.guardian_icon)
+                .noFade()
+                .into(imageView);
+
     }
 }
